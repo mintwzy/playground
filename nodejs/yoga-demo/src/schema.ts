687bb7c65e729cd715a175a7a699b711be91c655
mint.wzy <mint.wzy@gmail.com>
2022-07-20 10:59:53 +0800

    add some yoga demo code

--- nodejs/yoga-demo/src/schema.ts
+++ nodejs/yoga-demo/src/schema.ts
@@ -1,59 +1,82 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
+import type { GraphQLContext } from './context'
+import type { Link, Comment } from '@prisma/client'
 
 const typeDefinitions = /* GraphQL */ `
     type Query {
         info: String!
         feed: [Link!]!
+        comment(id: ID!): Comment
     }
 
     type Mutation {
         postLink(url: String!, description: String!): Link!
+        postCommentOnLink(linkId: ID!, body: String!): Comment!
     }
 
     type Link {
         id: ID!
         description: String!
         url: String!
+        comments: [Comment!]!
     }
 `
-
-// 1
-type Link = {
-  id: string
-  url: string
-  description: string
-}
-
-// 2
-const links: Link[] = [
-  {
-    id: 'link-0',
-    url: 'https://graphql-yoga.com',
-    description: 'The easiest way of setting up a GraphQL server',
-  },
-]
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 3
-    feed: () => links,
+    feed: async (parent: unknown, args: {}, context: GraphQLContext) => {
+      return context.prisma.link.findMany()
+    },
+    comment: async (
+      parent: unknown,
+      args: { id: string },
+      context: GraphQLContext,
+    ) => {
+      return context.prisma.comment.findUnique({
+        where: { id: parseInt(args.id) },
+      })
+    },
+  },
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
+    comments: (parent: Link, args: {}, context: GraphQLContext) => {
+      return context.prisma.comment.findMany({
+        where: {
+          linkId: parent.id,
+        },
+      })
+    },
   },
   Mutation: {
-    postLink: (parent: unknown, args: { description: string; url: string }) => {
-      // 1
-      let idCount = links.length
+    postLink: async (
+      parent: unknown,
+      args: { description: string; url: string },
+      context: GraphQLContext,
+    ) => {
+      const newLink = await context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
+    postCommentOnLink: async (
+      parent: unknown,
+      args: { linkId: string; body: string },
+      context: GraphQLContext,
+    ) => {
+      const newComment = await context.prisma.comment.create({
+        data: {
+          linkId: parseInt(args.linkId),
+          body: args.body,
+        },
+      })
 
-      // 2
-      const link: Link = {
-        id: `link-${idCount}`,
-        description: args.description,
-        url: args.url,
-      }
-
-      links.push(link)
-
-      return link
+      return newComment
     },
   },
 }
